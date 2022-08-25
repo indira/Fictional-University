1bf179964b061a4bc76e0586084cd7b4e9fd9530
Indira <indubhusal@gmail.com>
2022-08-25 15:27:46 -0700

    Completed Professor Post Type

--- wp-content/themes/Fictional-University/functions.php
+++ wp-content/themes/Fictional-University/functions.php
@@ -9,12 +9,20 @@ function university_files() {
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
-/*Menu*/
+
 function university_features() {
+ /*title-tag*/
   add_theme_support('title-tag');
+  /*Adding Post-thumbnails to the post type*/
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape',400,260, true);
+  add_image_size('professorPortrait',480,650,true);
+  add_image_size('pageBanner',1500,350,true);
+  /*Menu*/
   register_nav_menu('headerMenuLocation','Header Menu Location');
   register_nav_menu('footerLocationOne','Footer Location One');
   register_nav_menu('footerLocationTwo','Footer Location Two');
+  
 }
 
 add_action('after_setup_theme', 'university_features');
